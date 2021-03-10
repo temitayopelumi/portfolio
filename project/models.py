b896b33085ac419e6b35c31db1d34e3c950ceff8
temitayopelumi <olalekantemitayo7@gmail.com>
2021-03-10 07:20:35 +0100

    Adding content to Database

--- project/models.py
+++ project/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    technology = models.CharField(max_length=100)
+    image = models.FilePathField(path="/images")
